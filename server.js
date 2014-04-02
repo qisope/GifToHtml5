e71b917e652457ec2b9ba5197cbd774c0a1c8df8
Ross Anderson <qisope@radn.org>
2014-04-01 23:39:14 -0500

    quick support for graphicsmagic image resizing..hardcoded stuff

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var fs = require('fs');
 
 var restify = require('restify');
 var FFmpeg = require('fluent-ffmpeg');
+var gm = require('gm');
 
-function encode(req, res, next) {
+var encode = function(req, res, next) {
     var sourceUrl = req.params.url;
     var file = fs.createWriteStream('./file');
     var request = http.get(sourceUrl, function(response) {
@@ -19,18 +20,49 @@ function encode(req, res, next) {
                 .toFormat('webm')
                 .on('error', function(err) {
                     console.log('Error: ' + err.message);
+                    res.send('Failed');
                     next();
                 })
                 .on('end', function() {
                     console.log('Success');
+                    var rs = fs.createReadStream('./output.webm');
+                    rs.pipe(res);
                     next();
                 })
                 .saveToFile('./output.webm');
         });
     });
-}
+};
+
+var transformImage = function(req, res, next) {
+    var sourceUrl = req.params.url;
+    var file = fs.createWriteStream('./image');
+    var request = http.get(sourceUrl, function(response) {
+        console.log('Downloading source image...');
+        response.pipe(file);
+        file.on('finish', function() {
+            file.close();
+            console.log('Transcoding...');
+            gm('./image')
+                .resize(250, 250)
+                .write('./out.gif', function(err) {
+                    if (err) {
+                        console.log('Error: ' + err);
+                        res.send('Failed');
+                    }
+                    else {
+                        console.log('Success');
+                        var rs = fs.createReadStream('./out.gif');
+                        rs.pipe(res);
+                    }
+                    next();
+                });
+        });
+    });
+};
 
 var server = restify.createServer();
 server.get('/encode/:url', encode);
+server.get('/image/:url', transformImage);
 
 server.listen(3000, function() {});
